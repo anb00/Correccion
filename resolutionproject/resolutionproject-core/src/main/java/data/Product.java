9ff47fbb4a161ae5c557bc4ba5a26bacacef46ae
Antonio Nicolau Batle <32575391+anb00@users.noreply.github.com>
2018-11-14 18:40:03 +0100

    Add files via upload

---
+++ resolutionproject/resolutionproject-core/src/main/java/data/Product.java
@@ -0,0 +1,12 @@
+package data;
+
+import java.util.UUID;
+
+public abstract class Product {
+    UUID id = UUID.randomUUID(),
+    String name = name,
+    Double price = price ,
+    String description = description,
+    
+
+}
